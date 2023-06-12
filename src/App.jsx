a769010e9cbf978563fdc710847c9974ec6d1c62
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-12 00:22:09 -0700

    Init project -- Hotel Reservation App

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import logo from './assets/components/logo.png'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='wrapper'></div>
+        <div className='content'>
+      <div className='logo'>
+        <img src={logo} alt="" />
+      </div>
+      <div className='offer1'>
+        <span>
+        Become a member of La Réserve today to receive exclusive offers and great deals!
+        </span>
+        </div>
+        </div>
+        <div className="form">
+          <form action=""></form>
+        </div>
+   </div>
+    </>
+  )
+}
