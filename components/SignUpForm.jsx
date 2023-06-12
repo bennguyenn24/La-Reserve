0ab562369cb58f9a86dff60f3a8c5097f572dbe3
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-12 13:36:06 -0700

    Updated form and form validation

---
+++ components/SignUpForm.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import "../src/App.css";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+
+const SignUpForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email.includes('@')) {
+      setErrorMessage('Please enter a valid email address.');
+      return;
+    }
+
+    if (password.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      setErrorMessage('Password must be at least 8 characters long and contain a special character.');
+      return;
+    }
+
+    // Clear any previous error message
+    setErrorMessage('');
+
+    // Reset form fields
+    setEmail('');
+    setPassword('');
+
+    // Proceed with form submission logic
+    // ...
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="offer1">
+          <span>
+            Become a member of La Réserve today to receive exclusive offers and great deals!
+          </span>
+        </div>
+        {errorMessage && <p>{errorMessage}</p>}
+        <div className="form">
+          <label htmlFor="Sign up Form"></label>
+          <form onSubmit={handleSubmit}>
+            <div className="email-input">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email Address"
+                required
+              />
+            </div>
+            <div className="password-input">
+              <input 
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password" 
+                required
+              />
+            </div>
+            <div className="submit">
+              <button id="submit-btn" type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
 import './App.css'
-import logo from './assets/components/logo.png'
-import {faUser} from '@fortawesome/free-solid-svg-icons'
+import SignUpForm from '../components/signupForm'
 
 export default function App() {
 
   return (
     <>
       <div className='App'>
-        <div className='wrapper'></div>
-        <div className='content'>
-      <div className='logo'>
-        <img src={logo} alt="" />
-      </div>
-      <div className='offer1'>
-        <span>
-        Become a member of La Réserve today to receive exclusive offers and great deals!
-        </span>
-        </div>
-        <div className="form">
-          <form action="#">
-            <div className="email-input">
-            {/* <FontAwesomeIcon icon={faUser} /> */}
-            <input type="text" id="input" placeholder='Enter Email Address' />
-            </div>
-            <div className="password-input">
-            {/* <FontAwesomeIcon icon={faUser} /> */}
-            <input type="text" id="input" placeholder='Password' />
-            </div>
-            <div className="submit">
-              <button>Submit</button>
-            </div>
-          </form>
-        </div>
-        </div>
+        <SignUpForm />
    </div>
-    </>
+   </>
   )
 }
