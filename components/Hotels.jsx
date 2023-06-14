a071318d228b1ec8de8b46dc277baefc21fd264f
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-14 09:31:54 -0700

    Updated Changes

--- components/Hotels.jsx
+++ components/Hotels.jsx
@@ -5,22 +5,34 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBF8O4OR-mpiVchdYoRSKfcFG33yCmfEk",
-    authDomain: "la-reserve-4917a.firebaseapp.com",
-    projectId: "la-reserve-4917a",
-    storageBucket: "la-reserve-4917a.appspot.com",
-    messagingSenderId: "1075812642770",
-    appId: "1:1075812642770:web:598b0cc6ab44d24bec620b",
-    measurementId: "G-PRES5WF138"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const [bookDate, setBookDate] = useState("");
 const [bookTime, setBookTime] = useState("");
 const [leaveDate, setLeaveDate] = useState("");
 const [leaveTime, setLeaveTime] = useState("");
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (bookDate === "" || leaveDate === ""){
+        alert('Please select both book date and leave date.');
+        return;
+    }
+    if (bookTime === "" || leaveTime === "") {
+        console.log("Please select both book time and leave time.");
+        return;
+      }
+}
 
 const Hotels = () => {
   return (
@@ -29,7 +41,7 @@ const Hotels = () => {
         <div className="txt">Book Now</div>
         <div className="form">
           <div className="txt">Select Date & Time you would like to stay</div>
-          <form>
+          <form onSubmit={handleSubmit}> 
             <div className="inputData">
               <input
                 type="date"
@@ -66,7 +78,7 @@ const Hotels = () => {
               />
             </div>
             <div className="book">
-              <button type="submit">Book</button>
+              <button id="book-reservation-btn" type="submit" onSubmit={handleSubmit}>Book</button>
             </div>
           </form>
         </div>
