bf1680635a308c8b4a72d1ed6d0181421446b459
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-16 14:06:19 -0700

    Corrected Terminology

--- components/BookingForm.jsx
+++ components/BookingForm.jsx
@@ -1,22 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import "./Hotels.css";
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import "./BookingForm.css";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-const Hotels = () => {
+const BookingForm = () => {
     const [bookDate, setBookDate] = useState("");
     const [bookTime, setBookTime] = useState("");
     const [leaveDate, setLeaveDate] = useState("");
@@ -94,4 +80,4 @@ const Hotels = () => {
     );
 };
 
-export default Hotels;
+export default BookingForm;
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import SignUpForm from "../components/signupForm";
-import Hotels from "../components/Hotels";
+import BookingForm from "../components/BookingForm";
 import { useState } from "react";
 
 export default function App() {
 const [ signedUp , setSignedUp ] = useState(false);
-console.log(signedUp)
+
   return (
     <>
       <div className="App">
       {!signedUp ? (
         <SignUpForm setSignedUp={setSignedUp} />
       ) : (
-        <Hotels />
+        <BookingForm />
       )}
       </div>
     </>
