36177b6d5b62f773acfec85476015fc538b1b304
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-21 14:51:45 -0700

    Establish react routing

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
     res.send('Hello Mate')
 })
+
+// Routes
+app.use("/hotels", require("./routes/hotelRoutes"));
 
 app.listen(port, () => {
     console.log(`App is now running on port ${port} `)
