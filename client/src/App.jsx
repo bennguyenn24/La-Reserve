3ace1efb224762a0777ca4dbe6b6e0c06a399bd7
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-23 19:18:33 -0700

    Initiated React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,27 @@ import SignUpForm from "../components/SignUpForm";
 import BookingForm from "../components/BookingForm";
 import BookHotel from "../components/BookHotel";
 import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-const [ signedUp , setSignedUp ] = useState(false);
+    const [signedUp, setSignedUp] = useState(false);
 
-  return (
-    <>
-      <div className="App">
-       <BookHotel />
-      {!signedUp ? (
-        <SignUpForm setSignedUp={setSignedUp} />
-      ) : (
-        <BookingForm />
-      )}
-      </div>
-    </>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<BookHotel />}>
+                        <BookHotel />
+                        {!signedUp ? (
+                            <SignUpForm setSignedUp={setSignedUp} />
+                        ) : ( 
+                            <BookingForm />
+                        )}
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
