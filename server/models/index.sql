6616e37bdb589186e316e29c55f969e950ffdd20
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-28 16:58:02 -0700

    Updated table schema

--- server/models/index.sql
+++ server/models/index.sql
@@ -8,16 +8,17 @@ CREATE TABLE hotel (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
     price decimal(12,2),
-	description VARCHAR (255)
+	description VARCHAR (255),
+	image VARCHAR(255) NOT NULL
 );
 
 -- enter new data
-INSERT INTO hotel(name, price, description)
-VALUES ('The Grand Celestial Palace', 10000, 'Nestled amidst lush mountains, this opulent retreat offers breathtaking views, a private helipad, an infinity pool, and personalized butler service.' );
+INSERT INTO hotel(name, price, description, image)
+VALUES ('The Grand Celestial Palace', 10000, 'Nestled amidst lush mountains, this opulent retreat offers breathtaking views, a private helipad, an infinity pool, and personalized butler service.', 'https://res.cloudinary.com/dlahkgxj7/image/upload/v1687994392/DSRhotel_cek1dz.jpg');
 
-INSERT INTO hotel(name, price, description)
-VALUES ('The Diamond Sands Resort & Spa', 7500, 'Indulge in the ultimate beachfront experience at this lavish resort, boasting private cabanas, a world-class spa, gourmet dining, and a private yacht for exploration.' );
+INSERT INTO hotel(name, price, description, image)
+VALUES ('The Diamond Sands Resort & Spa', 7500, 'Indulge in the ultimate beachfront experience at this lavish resort, boasting private cabanas, a world-class spa, gourmet dining, and a private yacht for exploration.', 'https://res.cloudinary.com/dlahkgxj7/image/upload/v1687994392/GCPhotel_kngqkf.jpg');
 
-INSERT INTO hotel(name, price, description)
-VALUES ('The Platinum Heights Tower', 8000, 'Ascend to the epitome of urban elegance in this sleek skyscraper. Enjoy panoramic city views, a penthouse suite with a personal chef, a rooftop infinity pool, and exclusive access to a private club.' );
+INSERT INTO hotel(name, price, description, image)
+VALUES ('The Platinum Heights Tower', 8000, 'Ascend to the epitome of urban elegance in this sleek skyscraper. Enjoy panoramic city views, a penthouse suite with a personal chef, a rooftop infinity pool, and exclusive access to a private club.', 'https://res.cloudinary.com/dlahkgxj7/image/upload/v1687994392/PHThotel_cj1nmp.jpg');
 
