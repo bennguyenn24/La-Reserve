7e5266f324b7adcaaf3df3d764408409480f2bf3
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-03 11:49:45 -0700

    Deleted arrival and leaving time from booking form

--- client/components/BookingForm.jsx
+++ client/components/BookingForm.jsx
@@ -7,22 +7,20 @@ const BookingForm = ({ setBookingComplete }) => {
     };
 
     const [bookDate, setBookDate] = useState("");
-    const [bookTime, setBookTime] = useState("");
     const [leaveDate, setLeaveDate] = useState("");
-    const [leaveTime, setLeaveTime] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         // Validate form inputs
-        if (!bookDate || !bookTime || !leaveDate || !leaveTime) {
+        if (!bookDate || !leaveDate ) {
             alert("Please select all booking details.");
             return;
         }
 
         const currentDate = new Date();
-        const selectedBookDateTime = new Date(`${bookDate} ${bookTime}`);
-        const selectedLeaveDateTime = new Date(`${leaveDate} ${leaveTime}`);
+        const selectedBookDateTime = new Date(`${bookDate}T00:00:00`);
+        const selectedLeaveDateTime = new Date(`${leaveDate}T00:00:00`);
 
         // Check if booking time is in the past
         if (selectedBookDateTime < currentDate) {
@@ -43,45 +41,31 @@ const BookingForm = ({ setBookingComplete }) => {
     return (
         <div className="container">
             <div className="content">
-                <div className="txt">Book Now</div>
+                <div className="txt">Book now to see availability</div>
                 <div className="form">
                     <div className="txt">
-                        Select Date & Time you would like to stay
+                        Select Date you would like to stay
                     </div>
                     <form onSubmit={handleSubmit}>
                         <div className="inputData">
                             <input
                                 type="date"
                                 name="book-date"
+                                min={new Date().toISOString().split("T")[0]}
                                 id="book-date"
                                 onChange={(e) => setBookDate(e.target.value)}
                             />
                         </div>
-                        <div className="inputData">
-                            <input
-                                type="time"
-                                name="book-time"
-                                id="book-time"
-                                onChange={(e) => setBookTime(e.target.value)}
-                            />
-                        </div>
                         <div className="txt">
-                            Select Date & Time you would like to leave.
+                            Select Date you would like to leave.
                         </div>
                         <div className="inputData">
                             <input
                                 type="date"
                                 name="leave-date"
+                                min={bookDate}
                                 id="leave-date"
                                 onChange={(e) => setLeaveDate(e.target.value)}
-                            />
-                        </div>
-                        <div className="inputData">
-                            <input
-                                type="time"
-                                name="leave-time"
-                                id="leave-time"
-                                onChange={(e) => setLeaveTime(e.target.value)}
                             />
                         </div>
                         <div className="book">
