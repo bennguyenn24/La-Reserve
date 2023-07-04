c0af87cc6f037868ad63eafab83bd4e76335092b
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-04 15:31:15 -0700

    Handle bookings

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
+const db = require("./db/index");
 const app = express();
 var cors = require("cors");
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello Mate");
 });
 
 // Handle Bookings
-app.post("/bookings", (req, res) => {
+app.post("/bookings", async (req, res) => {
     const { userEmail, bookDate, leaveDate } = req.body;
-    const query = INSERT INTO bookings(userEmail, bookDate, leaveDate)
-    VALUES ($1, $2, $3)) 
-})
+    const { rows } = await db.query(
+        "INSERT INTO bookings(email, price, created, book_date, leave_date) VALUES ($1, $2, DEFAULT, $3, $4) RETURNING *",
+        [userEmail, 10000, bookDate, leaveDate]
+    );
 
+    res.json({ success: true, data: rows[0] });
+});
 // Routes
 app.use("/hotels", require("./routes/hotelRoutes"));
 
