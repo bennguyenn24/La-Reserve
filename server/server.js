44025fff42cae9d85a16893bb2f928b12d7a4b49
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-13 22:45:12 -0700

    Connected Stripe API to backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const db = require("./db/index");
 const app = express();
+require("dotenv").config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+console.log(process.env.STRIPE_SECRET_KEY)
+
+
+const YOUR_DOMAIN = "http://localhost:3000";
 
 var cors = require("cors");
 const port = 3000;
@@ -25,6 +30,26 @@ app.post("/bookings", async (req, res) => {
 });
 // Routes
 app.use("/hotels", require("./routes/hotelRoutes"));
+
+// Stripe
+app.post('/create-checkout-session', async (req, res) => {
+    console.log("hit")
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1NTXjDCA7YJyiVj9rRvnrJkO',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+  
+    res.redirect(303, session.url);
+  });
+  
 
 app.listen(port, () => {
     console.log(`App is now running on port ${port} `);
