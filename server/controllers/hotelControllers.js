23400ea7341c6efe4fb12d1b3eb35231aea530fc
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-21 13:18:54 -0700

    Added orderBy to query

--- server/controllers/hotelControllers.js
+++ server/controllers/hotelControllers.js
@@ -1,7 +1,7 @@
 const db = require("../db/index");
 
 async function getAllHotels(req, res) {
-    const { rows } = await db.query("SELECT * from hotel;");
+    const { rows } = await db.query("SELECT * from hotel ORDER BY name ASC");
 
     res.json({
         data: rows
