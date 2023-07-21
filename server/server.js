b05a6068545fa48db523205649dbdeabc8bcad14
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-21 13:59:43 -0700

    Updated checkout to use real data

--- server/server.js
+++ server/server.js
@@ -31,28 +31,43 @@ app.post("/bookings", async (req, res) => {
 app.use("/hotels", require("./routes/hotelRoutes"));
 
 // Stripe
-app.post("/create-checkout-session", async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price: "price_1NV0NFCA7YJyiVj9Et8l5LlX",
-                quantity: 1,
-            },
-            {
-              price: "price_1 NUzqDCA7YJyiVj9AwMQLVLy",
-                quantity: 1,
-            },
-            {
-              price: "price_1NUzylCA7YJyiVj9ZDdlfLYz",
-                quantity: 1,
-            }
-        ],
-        mode: "payment",
-        success_url: `${YOUR_DOMAIN}?success=true`,
-        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-    });
+// Need to pull and fetch data from the selected hotel
+// create checkout session
+// fetch hotel information
 
-    res.redirect(303, session.url);
+app.post("/create-checkout-session", async (req, res) => {
+    try {
+        const { hotelId } = req.body;
+
+        const { rows } = await db.query(
+            "SELECT name, price, description, image, price_id FROM hotel WHERE id = $1",
+            [Number(hotelId)]
+        );
+
+        if (rows.length > 0) {
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price: rows[0].price_id,
+                        quantity: 1,
+                    },
+                ],
+                mode: "payment",
+                success_url: `${YOUR_DOMAIN}?success=true`,
+                cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+            });
+
+            res.json({session});
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Hotel not found",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(port, () => {
