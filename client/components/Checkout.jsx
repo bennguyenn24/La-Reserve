3980d1b8fb7992004d006f9e54978dc496b5b47f
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-24 23:31:28 -0700

    No need for Checkout page, implemented stripe

--- client/components/Checkout.jsx
+++ client/components/Checkout.jsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import "./Checkout.css";
+// import React from 'react'
+// import "./Checkout.css";
 
 
-const Checkout = ({handleBookNow}) => {
+// const Checkout = ({handleBookNow}) => {
 
-  return (
-    <div>
-        <div className="checkout-container">
-            <h1 className='container-title'>Checkout</h1>
-            <form className="checkout-form">
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" className="form-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" className="form-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="address">Address:</label>
-          <input type="text" id="address" className="form-input" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="cardNumber">Card Number:</label>
-          <input type="text" id="cardNumber" className="form-input" />
-        </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="expiryDate">Expiry Date:</label>
-            <input type="text" id="expiryDate" className="form-input" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="cvv">CVV:</label>
-            <input type="text" id="cvv" className="form-input" />
-          </div>
-        </div>
-        <button className="book-now-button" onClick={handleBookNow}>
-        Book Now
-      </button>
-      </form>
-        </div>
+//   return (
+//     <div>
+//         <div className="checkout-container">
+//             <h1 className='container-title'>Checkout</h1>
+//             <form className="checkout-form">
+//         <div className="form-group">
+//           <label htmlFor="name">Name:</label>
+//           <input type="text" id="name" className="form-input" />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="email">Email:</label>
+//           <input type="email" id="email" className="form-input" />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="address">Address:</label>
+//           <input type="text" id="address" className="form-input" />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="cardNumber">Card Number:</label>
+//           <input type="text" id="cardNumber" className="form-input" />
+//         </div>
+//         <div className="form-row">
+//           <div className="form-group">
+//             <label htmlFor="expiryDate">Expiry Date:</label>
+//             <input type="text" id="expiryDate" className="form-input" />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="cvv">CVV:</label>
+//             <input type="text" id="cvv" className="form-input" />
+//           </div>
+//         </div>
+//         <button className="book-now-button" onClick={handleBookNow}>
+//         Book Now
+//       </button>
+//       </form>
+//         </div>
    
-  </div>
-);
-}
+//   </div>
+// );
+// }
 
-export default Checkout;
+// export default Checkout;
