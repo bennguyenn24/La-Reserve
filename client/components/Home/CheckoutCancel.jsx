996a953c9e346a81cfb3f396671a299e3b8e1fc5
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-25 18:41:30 -0700

    Added Checkout success and cancel

---
+++ client/components/Home/CheckoutCancel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckoutCancel = () => {
+  return (
+    <div>CheckoutCancel</div>
+  )
+}
+
+export default CheckoutCancel
---
+++ client/components/Home/CheckoutSuccess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckoutSuccess = () => {
+  return (
+    <div>
+        Checkout success
+    </div>
+  )
+}
+
+export default CheckoutSuccess
