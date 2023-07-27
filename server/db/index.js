ec560c1024130c8adddd2a46d8d42d5838963664
Ben Nguyen <bennguyenn24@gmail.com>
2023-07-27 13:24:24 -0700

    Updated pg to take vercel psql

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ require("dotenv").config({path:"./.env"});
 
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({connectionString: process.env.POSTGRES_URL + "?sslmode=require",});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
