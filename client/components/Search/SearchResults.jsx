bbab70ebf61e29e7ee9a66b8491e046490abd6d0
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-01 14:23:53 -0700

    Updated and stylized Search results

--- client/components/Search/SearchResults.jsx
+++ client/components/Search/SearchResults.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { HotelsContext } from "../../context/HotelsContext";
 import HotelCard from "../Home/HotelCard";
+import "./SearchResults.css";
 
 const SearchResults = () => {
     const { location } = useParams();
@@ -13,22 +14,31 @@ const SearchResults = () => {
 
     return (
         <div>
-            <h1>Search Results</h1>
-            <p>Location: {location}</p>
-            <section className="products__container">
-                {hotelsFilteredByLocation.map((hotel) => (
-                    <HotelCard
-                        id={hotel.id}
-                        title={hotel.title}
-                        location={hotel.location}
-                        image={hotel.image}
-                        price={hotel.price}
-                        description={hotel.description}
-                    />
-                ))}
-            </section>
+            {/* Ternary operator to conditionally render search results or "No Hotels Found" */}
+            {hotelsFilteredByLocation.length === 0 ? (
+                <div className="no-hotel-container">
+                    <h1 className="header1">No Hotels Found in this area</h1>
+                    <button>Go Back</button>
+                    </div>
+            ) : (
+                <>
+                    <h1>Search Results</h1>
+                    <p>Location: {location}</p>
+                    <section className="products__container">
+                        {hotelsFilteredByLocation.map((hotel) => (
+                            <HotelCard
+                                key={hotel.id}
+                                title={hotel.title}
+                                location={hotel.location}
+                                image={hotel.image}
+                                price={hotel.price}
+                                description={hotel.description}
+                            />
+                        ))}
+                    </section>
+                </>
+            )}
         </div>
     );
 };
-
 export default SearchResults;
