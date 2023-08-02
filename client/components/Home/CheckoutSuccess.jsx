36b8fff78cbc1be45da78b146ae5c99038454068
Ben Nguyen <bennguyenn24@gmail.com>
2023-08-02 15:03:37 -0700

    Added sessionInfo

--- client/components/Home/CheckoutSuccess.jsx
+++ client/components/Home/CheckoutSuccess.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const CheckoutSuccess = () => {
-  return (
-    <div>
-        Checkout success
-    </div>
-  )
-}
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
 
-export default CheckoutSuccess
+    const [sessionInfo, setSessionInfo] = useState({
+        amount: "",
+        customer: "",
+        status: "",
+    });
+
+    // You can now access the individual query parameters using the get() method of URLSearchParams
+    const sessionId = searchParams.get("session_id");
+
+    // Fetch session info
+    useEffect(() => {
+        async function getSessionInfo() {
+            const res = await axios.get(
+                `https://la-reserve-server.onrender.com/order?session_id=${sessionId}`
+            );
+            setSessionInfo(res.data.sessionInfo)
+        }
+
+        getSessionInfo();
+    }, []);
+
+    return (
+        <div>
+            Checkout success
+            <p>Confirmation ID: {sessionId}</p>
+            <p>Customer: {sessionInfo.customer}</p>
+            <p>Amount: {sessionInfo.amount}</p>
+            <p>Status: {sessionInfo.status}</p>
+        </div>
+    );
+};
+
+export default CheckoutSuccess;
