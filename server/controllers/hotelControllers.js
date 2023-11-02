6d3405c70d176e73faa0ae78d69dfffb927e37bb
Philip Nguyen <philipnguyen953@gmail.com>
2023-11-02 16:43:14 -0700

    Created getHotelById controller and route

--- server/controllers/hotelControllers.js
+++ server/controllers/hotelControllers.js
@@ -1,11 +1,23 @@
 const db = require("../db/index");
 
 async function getAllHotels(req, res) {
-    const { rows } = await db.query("SELECT * from hotel ORDER BY name ASC");
+  const { rows } = await db.query("SELECT * from hotel ORDER BY name ASC");
 
-    res.json({
-        data: rows
-    });
+  res.json({
+    data: rows,
+  });
 }
 
-module.exports = {getAllHotels}
+async function getHotelById(req, res) {
+  const { hotelId } = req.params;
+
+  const { rows } = await db.query("SELECT * from hotel WHERE id = $1", [
+    Number(hotelId),
+  ]);
+
+  res.json({
+    data: rows[0],
+  });
+}
+
+module.exports = { getAllHotels, getHotelById };
